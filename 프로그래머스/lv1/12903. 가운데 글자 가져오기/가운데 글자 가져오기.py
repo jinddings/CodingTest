e3d08222571a497e3275d816d1e526c6a105d743
jinddings <86943988+jinddings@users.noreply.github.com>
2023-06-17 23:56:02 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = ''
+    mid = 0
+    if len(s) % 2 == 1 :
+        answer = s[len(s)//2]
+    else :
+        answer = s[len(s)//2-1 : len(s)//2+1]
+        
+    return answer

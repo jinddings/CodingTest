345494bba3a4c4f2efd40c6fa5a821ddefa9d571
pjm2476 <pjm2476@gmail.com>
2024-06-14 17:59:16 +0900

    [프로그래머스]  행렬의 곱셈

---
+++ 프로그래머스/lv2/행렬의 곱셈.py
@@ -0,0 +1,8 @@
+def solution(arr1, arr2):
+    answer = [[0] * len(arr2[0]) for _ in range(len(arr1))]
+    for i in range(len(arr1)):
+        for k in range(len(arr2[0])):
+            for j in range(len(arr1[0])):
+                answer[i][k] += arr1[i][j] * arr2[j][k]
+
+    return answer

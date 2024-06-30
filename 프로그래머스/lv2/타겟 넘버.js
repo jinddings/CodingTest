39757a0b76d8b99d1a361b5c8e755b0ce1bdbee9
pjm2476 <pjm2476@gmail.com>
2024-06-30 15:23:29 +0900

    [프로그래머스] 타겟 넘버

---
+++ 프로그래머스/lv2/타겟 넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+  var answer = 0;
+  dfs(0, 0);
+
+  function dfs(idx, sum) {
+    if (idx === numbers.length) {
+      if (sum === target) {
+        answer++;
+      }
+      return;
+    }
+
+    dfs(idx + 1, sum + numbers[idx]);
+    dfs(idx + 1, sum - numbers[idx]);
+  }
+  return answer;
+}
